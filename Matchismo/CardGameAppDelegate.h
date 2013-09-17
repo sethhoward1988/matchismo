e2804b91c2f5f3d70d1ac5cac04c08b1b7cea25e
Seth Howard <smiles.seth@gmail.com>
2013-09-17 12:52:48 -0600

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Seth Howard on 9/17/13.
+//  Copyright (c) 2013 Seth Howard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Seth Howard on 9/17/13.
+//  Copyright (c) 2013 Seth Howard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
