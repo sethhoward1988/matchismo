c6c2cafd6c3c3f5732cf0de13c3f61296caf1eac
Seth Howard <smiles.seth@gmail.com>
2013-09-22 10:29:56 -0600

    End of lecture 3

---
+++ Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,22 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Seth Howard on 9/22/13.
+//  Copyright (c) 2013 Seth Howard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+- (id)initWithCardCount:(NSUInteger)cardCount usingDeck:(Deck *)deck;
+
+- (void)flipCardAtIndex:(NSUInteger)index;
+
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly) int score;
+
+@end
